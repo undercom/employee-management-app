1332373b39b8a35e1cf38d3519b57c85a6b0aa24
Claudiu Hojda <djhojd@gmail.com>
2019-03-17 16:20:56 +0200

    Add Reverse pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { SearchComponent } from './components';
+import { ReversePipe } from './pipes';
 
 @NgModule({
-  declarations: [SearchComponent],
-  exports: [SearchComponent],
+  declarations: [SearchComponent, ReversePipe],
+  exports: [SearchComponent, ReversePipe],
 })
 export class SharedModule {}
