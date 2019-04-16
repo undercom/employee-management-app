8ea9d5a8d09df5f4ab10be6f98c5b26a12059363
Claudiu Hojda <djhojd@gmail.com>
2019-04-16 18:46:22 +0300

    Replace triggerIfEmpty with placeholderText

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -5,13 +5,13 @@ import { Component, Output, Input, EventEmitter } from '@angular/core';
   templateUrl: './search.component.html',
 })
 export class SearchComponent {
-  @Input() triggerIfEmpty: boolean;
+  @Input() placeholderText: string;
 
   @Output() textChanged = new EventEmitter<string>();
 
   onKeyUp(newText: string) {
-    const isEmpty = newText && newText.length > 0;
-    if (!isEmpty || this.triggerIfEmpty) {
+    const isEmpty = newText.length > 0;
+    if (!isEmpty) {
       this.textChanged.next(newText);
     }
   }
