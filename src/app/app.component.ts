c40c168448b7ad527c225faf90ddeaee67319f64
Sebastian Mihalca <sebastian.mihalca@visma.com>
2019-04-17 13:54:31 +0300

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,4 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'app works!';
-
-  userObj = {
-    username: 'foobar',
-    firstName: 'Foo',
-    lastName: 'Bar',
-    age: 18,
-    isAdmin: false,
-  };
-
-  onChange(value) {
-    console.log('APP', value);
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { EmployeeListComponent} from './list.component';
+import { EmployeeListComponent } from './list.component';
 
 @NgModule({
   declarations: [AppComponent, EmployeeListComponent],
