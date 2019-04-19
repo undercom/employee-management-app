a707212691002db5143f57227554df32f2e3119f
Sebastian Mihalca <sebastian.mihalca@visma.com>
2019-04-19 11:22:55 +0300

    Stream

--- src/app/employee/components/employee-list/employee-list.component.ts
+++ src/app/employee/components/employee-list/employee-list.component.ts
@@ -1,33 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject, combineLatest } from 'rxjs';
 import {
   EmployeeService,
   Employee,
 } from '../../../services/employee/employee.service';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  startWith,
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
 })
 export class EmployeeListComponent implements OnInit {
-  // employees =
-
   employees: Employee[];
+
+  searchStream$ = new Subject<string>();
+  devStream$ = new Subject<boolean>();
 
   constructor(private employeeService: EmployeeService) {
     this.employeeService = employeeService;
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.employees = this.employeeService.getEmployees();
     this.employeeFiltred = this.employees;
+
+    const filteredSearch = this.searchStream$.pipe(
+      startWith(''),
+      debounceTime(300),
+      distinctUntilChanged(),
+      filter(searchText => searchText.length > 2 || searchText.length === 0)
+    );
+
+    const filteredDev$ = this.devStream$.pipe(startWith(false));
+
+    combineLatest(filteredSearch, filteredDev$).subscribe(val =>
+      console.log(val)
+    );
   }
 
   employeeFiltred = [];
 
-  onSearch(value: string) {
-    this.employeeFiltred = this.employees.filter(emp =>
-      emp.name.toLowerCase().includes(value.toLowerCase())
-    );
-    console.log('Output: ', value, this.employeeFiltred);
+  onSearch(value: string = '', onlyDevs: boolean) {
+    console.log('Filtering employee', value, onlyDevs);
+    this.employees = this.employees.filter((employee: Employee) => {
+      const matchName =
+        value.length === 0 ||
+        employee.name.toLocaleLowerCase().includes(value.toLocaleLowerCase());
+      const isDev = !onlyDevs || employee.position.includes('dev');
+      return matchName && isDev;
+    });
   }
+
+  // onSearch(value: string) {
+  //   this.employeeFiltred = this.employees.filter(emp =>
+  //     emp.name.toLowerCase().includes(value.toLowerCase())
+  //   );
+  //   console.log('Output: ', value, this.employeeFiltred);
+  // }
 }
